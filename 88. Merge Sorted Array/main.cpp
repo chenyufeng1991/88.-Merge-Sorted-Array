6cff1d06d87eabc1f615c60478f675f09f245b0e
陈宇峰 <yufengcode@gmail.com>
2016-10-05 10:06:05 +0800

    Initial Commit

---
+++ 88. Merge Sorted Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  88. Merge Sorted Array
+//
+//  Created by chenyufeng on 10/5/16.
+//  Copyright © 2016 chenyufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
